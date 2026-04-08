01e00b5c1cd587e40182815a2632e47edd287195
lide <lide2591@163.com>
2026-04-08 18:04:39 +0800

    test-01

---
+++ parent/provider/src/main/java/com/lide/maven/TestController.java
@@ -0,0 +1,15 @@
+package com.lide.maven;
+
+/**
+ * Classname: TestConroller
+ * Package: com.lide.maven
+ * Description:
+ *
+ * @Date: 2026-04-08 18:02
+ * @Author: lide
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
